6f7e028dc8d0ca7697c59ba376a8fd59bf4603ce
Beyza Özmaden <beyzaozmaden@gmail.com>
2023-04-08 01:48:56 +0300

    Health Bar Added

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -1,18 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float health, animationSlowness;
+    private float maxHealth, realScale;
+
+
     void Start()
     {
-        
+        maxHealth = health;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        realScale = health / maxHealth;
+
+        if (transform.localScale.x > realScale)
+        {
+            transform.localScale = new Vector3(transform.localScale.x - (transform.localScale.x-realScale) / animationSlowness, transform.localScale.y, transform.localScale.z);
+        }
+
+        if (transform.localScale.x < realScale)
+        {
+            transform.localScale = new Vector3(transform.localScale.x + (realScale - transform.localScale.x) / animationSlowness, transform.localScale.y, transform.localScale.z);
+        }
+
+        if (Input.GetKeyDown("a") && health>0)
+        {
+            health -= 10;
+        }
+
+        if (Input.GetKeyDown("s") && health > 0)
+        {
+            health -= 5;
+        }
+
+        if (Input.GetKeyDown("d") && health > 0)
+        {
+            health -= 1;
+        }
+
+        if (Input.GetKeyDown("r") && health < maxHealth)
+        {
+            health = maxHealth;
+        }
+
+        if (health < 0)
+        {
+            health = 0;
+        }
+
+        if (health > maxHealth)
+        {
+            health = maxHealth;
+        }
     }
 }
